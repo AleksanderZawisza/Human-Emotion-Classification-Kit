65981d3996b96dac77436f6895c857d81a48af5b
AleksanderZawisza <aleksanderpodsiad@wp.pl>
2021-12-03 19:33:20 +0100

    main + utils

--- app-sg/main.py
+++ app-sg/main.py
@@ -1,5 +1,72 @@
+import PySimpleGUI as sg
 
+
+# from utils import ColumnFixedSize
+
+
+class HECKApp:
+    def __init__(self, *args, **kwargs):
+        sg.theme("DefaultNoMoreNagging")
+        sg.theme_button_color(('blue', '#b8cde0'))
+        sg.set_options(font=('Courier New', 10))
+
+        # ----------- Create all the layouts this Window will display -----------
+        layout1 = [[sg.Text('Human Emotion Classification Kit', font=('Courier New', 25), pad=((0, 0), (70, 0)))],
+                   [sg.HSep(pad=((0, 0), (0, 20)))],
+                   [sg.Button('Prediction demo', size=(30, 2), pad=((0, 0), (50, 0)), font=('Courier New', 14))],
+                   [sg.Button('Load images', size=(30, 2), pad=((0, 0), (50, 0)), font=('Courier New', 14))],
+                   [sg.Button('Prediction settings', size=(30, 2), pad=((0, 0), (50, 0)), font=('Courier New', 14))]]
+
+        layout2 = [[sg.Text('Load Page interior')],
+                   [sg.Button('Back')]]
+
+        layout3 = [[sg.Text('Change prediction settings', font=('Courier New', 20))],
+                   [sg.Frame('Choose model',
+                             [[sg.Radio("Model 1", group_id=1, default=True)],
+                              [sg.Radio("Model 2", group_id=1)],
+                              [sg.Radio("Model 3", group_id=1)]], expand_x=True, pad=((0, 0), (50, 0)),
+                             font=('Courier New', 12))],
+                   [sg.Frame('Use face detection?',
+                             [[sg.Radio("Yes", group_id=2, default=True)],
+                              [sg.Radio("No", group_id=2)]], expand_x=True, pad=((0, 0), (50, 0)),
+                             font=('Courier New', 12))],
+                   [sg.Button('Back', pad=((0, 0), (50, 0)), size=(10, 1), font=('Courier New', 14))]]
+
+        layout4 = [[sg.Text('Prediction Page interior')],
+                   [sg.Button('Back')]]
+
+        # ----------- Create actual layout using Columns and a row of Buttons -----------
+        layout = [[sg.Column(layout1, key='-COL1-', element_justification='center', vertical_alignment='c',
+                             expand_y=True),
+                   sg.Column(layout2, visible=False, key='-COL2-', element_justification='center'),
+                   sg.Column(layout3, visible=False, key='-COL3-', element_justification='center'),
+                   sg.Column(layout4, visible=False, key='-COL4-', element_justification='center')]]
+
+        window = sg.Window('Human Emotion Classification Kit', layout, element_justification='center',
+                           size=(800, 600), finalize=True)
+
+        layout = 1
+        while True:
+            event, values = window.read()
+            print(event, values)
+            if event in (None, 'Exit'):
+                break
+            if event == 'Prediction demo':
+                window[f'-COL1-'].update(visible=False)
+                window[f'-COL4-'].update(visible=True)
+            if event == 'Load images':
+                window[f'-COL1-'].update(visible=False)
+                window[f'-COL2-'].update(visible=True)
+            if event == 'Prediction settings':
+                window[f'-COL1-'].update(visible=False)
+                window[f'-COL3-'].update(visible=True)
+            if 'Back' in event:
+                window[f'-COL2-'].update(visible=False)
+                window[f'-COL3-'].update(visible=False)
+                window[f'-COL4-'].update(visible=False)
+                window[f'-COL1-'].update(visible=True)
+        window.close()
 
 
 if __name__ == "__main__":
-    print('Hello world')
+    app = HECKApp()
